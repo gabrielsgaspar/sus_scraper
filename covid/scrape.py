c953a91864726c88256c274ed65b89b21889e853
Gabriel Simoes Gaspar <gabrielsgaspar@gmail.com>
2020-11-13 18:03:55 +0000

    Set structure

---
+++ covid/scrape.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+This scrip scrapes data from LEITOS API in DataSUS in order to gather data for
+daily infections and deaths from Covid-19.
+
+By: @gabrielsgaspar
+"""
+
+# Import libraries
+
+# Define function to paginate through the API
+def api_paginate():
+    """
+    Description
+    """
+
+# Define function to flatten data and store in dataframe
+def json_df():
+    """
+    Description
+    """
+
+# Define main call function
+def main():
+    """
+    Description
+    """
+
+if __name__ == '__main__':
+    main()
